79c937ac20d862ed608bfea83625d534ffd0a4a1
Vytautas-Pranskunas <vytautas@devbridge.com>
2013-03-16 16:56:29 -0700

    aaaa

--- MySavings/pages/itemDetail/itemDetail.js
+++ MySavings/pages/itemDetail/itemDetail.js
@@ -5,14 +5,24 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
-            var item = options && options.item ? Data.resolveItemReference(options.item) : Data.items.getAt(0);
-            element.querySelector(".titlearea .pagetitle").textContent = item.group.title;
-            element.querySelector("article .item-title").textContent = item.title;
-            element.querySelector("article .item-subtitle").textContent = item.subtitle;
-            element.querySelector("article .item-image").src = item.backgroundImage;
-            element.querySelector("article .item-image").alt = item.subtitle;
-            element.querySelector("article .item-content").innerHTML = item.content;
-            element.querySelector(".content").focus();
+            //debugger;
+            
+            var tableBody = document.getElementById("myTableBody");
+            var template = document.getElementById("myTemplate").winControl;
+            //debugger;
+            var b = Db.getBudget(options.key);
+            b.income.forEach(function (item) {
+                template.render(item, tableBody);
+            });
+
+            //var item = options && options.item ? Data.resolveItemReference(options.item) : Data.items.getAt(0);
+            //element.querySelector(".titlearea .pagetitle").textContent = item.group.title;
+            element.querySelector("article .item-title").textContent = b.title;
+            //element.querySelector("article .item-subtitle").textContent = item.subtitle;
+            //element.querySelector("article .item-image").src = item.backgroundImage;
+            //element.querySelector("article .item-image").alt = item.subtitle;
+            //element.querySelector("article .item-content").innerHTML = item.content;
+            //element.querySelector(".content").focus();
         }
     });
 })();
