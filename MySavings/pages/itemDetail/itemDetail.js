4cdde94a841b0bd70afa68c3f5a048a9f119be80
irmastnt <irmastnt@gmail.com>
2013-03-17 14:26:36 +0200

    Added Item Detail Title

--- MySavings/pages/itemDetail/itemDetail.js
+++ MySavings/pages/itemDetail/itemDetail.js
@@ -17,6 +17,7 @@
             self = this;
             listView = document.querySelector(".incomeItemsList").winControl;
             budget = Db.getBudget(options.key);
+            document.querySelector('.pagetitle').innerHTML = budget.title;
             
             MS.init(appbar.winControl,
                 listView,
